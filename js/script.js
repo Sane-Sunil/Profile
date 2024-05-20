36ed83ec7b7044c0f0c487517a5f39a7dc685856
Sane-Sunil <sanesunil.falseofficial@gmail.com>
2024-05-20 10:38:30 +0530

    index done

--- js/script.js
+++ js/script.js
@@ -3,6 +3,6 @@ document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("body").innerHTML = `<p id='redirect'>Click <a href="profile.html">Redirect</a> if it doesn't automatically redirect</p>`;
     setTimeout(function(){
         document.head.innerHTML = `<meta http-equiv="refresh" content="0; url=profile.html" />`
-    }, 3000);
+    }, 1000);
     
 });
