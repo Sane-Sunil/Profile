904f28fddc116820fe65d6cfe24f09c12d335fac
Sane-Sunil <sanesunil.falseofficial@gmail.com>
2024-05-24 19:59:40 +0530

    simple_js

--- js/script.js
+++ js/script.js
@@ -3,21 +3,21 @@ document.addEventListener("DOMContentLoaded", function(){
     // ~####    Random    ####~ \\
 
     // let len = ab.length();
-    let ab = ['profile1.html', 'profile2.html', 'profile3.html'];
-    let rand = Math.floor(Math.random() * 3);
-    let randhtml = ab[rand];
-    document.body.style.backgroundImage = "none";
-    document.getElementById("body").innerHTML = `<p id='redirect'>Click <a href="profile.html">Redirect</a> if it doesn't automatically redirect</p>`;
-    setTimeout(function(){
-        document.head.innerHTML = `<meta http-equiv="refresh" content="0; url=${randhtml}" />`
-    }, 1000);
-
-    // ~####    Simple    ####~ \\
-
+    // let ab = ['profile1.html', 'profile2.html', 'profile3.html'];
+    // let rand = Math.floor(Math.random() * 3);
+    // let randhtml = ab[rand];
     // document.body.style.backgroundImage = "none";
     // document.getElementById("body").innerHTML = `<p id='redirect'>Click <a href="profile.html">Redirect</a> if it doesn't automatically redirect</p>`;
     // setTimeout(function(){
-    //     document.head.innerHTML = `<meta http-equiv="refresh" content="0; url=profile1.html" />`
+    //     document.head.innerHTML = `<meta http-equiv="refresh" content="0; url=${randhtml}" />`
     // }, 1000);
+
+    // ~####    Simple    ####~ \\
+
+    document.body.style.backgroundImage = "none";
+    document.getElementById("body").innerHTML = `<p id='redirect'>Click <a href="profile.html">Redirect</a> if it doesn't automatically redirect</p>`;
+    setTimeout(function(){
+        document.head.innerHTML = `<meta http-equiv="refresh" content="0; url=profile1.html" />`
+    }, 1000);
     
 });
